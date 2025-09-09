dac18df4e218c72ea86f9d5dcb855ddecc0469ff
wesleygabri <wesleygabri96@gmail.com>
2025-09-08 22:51:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.getElementById('meuBotao').addEventListener('click', () => {
-  alert('Você clicou no botão!');
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const usuario = document.getElementById('usuario').value;
+  const senha = document.getElementById('senha').value;
+
+  if (usuario === 'admim' && senha === 'Mm102030') {
+    // Login correto → redireciona
+    window.location.href = 'painel.html';
+  } else {
+    // Erro
+    alert('Usuário ou senha inválidos!');
+  }
 });
